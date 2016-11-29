53a3a21cddc0e6f7da7246a57a8d7a893717f0cc
Chung Oi Man <uhmon@Chungs-MacBook-Pro.local>
2016-11-30 01:48:05 +0800

    with JS conditions

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,3 +5,15 @@ var cardTwo = "queen"
 var cardThree = "king"
 var cardFour = "king"
 
+if (cardOne === cardTwo || cardThree === cardFour){
+alert("You found a match!");
+} else if (cardOne === cardThree){
+	alert("Sorry, try again.");
+} else if (cardOne === cardFour){
+	alert("Sorry, try again.");
+} else if (cardTwo === cardThree){
+	alert("Sorry, try again.");
+} else if(cardTwo === cardFour){
+	alert("Sorry, try again.");
+}
+
